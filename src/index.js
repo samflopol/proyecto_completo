b5837872bc4c45ceb5372b3ddb98ccc79163cab1
samflopol <sflorez@uniempresarial.edu.co>
2024-05-20 19:49:33 -0500

    inicio pagina

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 /* -------------------------- importar dependencias ------------------------- */
 import express from 'express'
+import morgan from 'morgan';
+
+import{join,dirname} from 'path'
+import{fileURLToPath} from 'url'
+import{engine}from 'express-handlebars'
+import exp from 'constants';
+
 
 /* ----------------------------- initializacion ----------------------------- */
 const app = express();
+/* ---------------------------- ecitar coliciones --------------------------- */
+const __dirname=dirname(fileURLToPath(import.meta.url)) ;
+
 /* --------------------------------- setting -------------------------------- */
 app.set('port',process.env.PORT || 3000)
+
+app.set('views',join (__dirname,'views'))
+
+app.engine('.hbs',engine({
+    defaultLayout:'main',
+    layoutsDir:join(app.get('views'),'layouts'),
+    partialsDir:join(app.get('views'),'partials'),
+    extname:'.hbs',
+
+}))
+app.set('view engine', '.hbs');
+
+
 /* ------------------------------- middlewares ------------------------------ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 /* --------------------------------- routes --------------------------------- */
 
+app.get('/',(req,res)=>{
+res.render('index')
+});
+
 /* ------------------------------ public files ------------------------------ */
+app.use(express.static(join(__dirname,'public')));
 
 /* ------------------------------- run server ------------------------------- */
 app.listen(app.get('port'),()=>{
